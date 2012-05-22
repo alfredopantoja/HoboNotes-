f2e54bb777ee446482a9cc7394b16fa6ecab008a
Alfredo Pantoja <chololoy@gmail.com>
2012-05-22 14:42:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hobonotes::Application.config.secret_token = 'ccd6e538e1ba58e4143488bb6ef724f57ab13f640499b6df2f1688006e5b8bab3f8551fa453e7d823c3719d4fa5ac925ada597c4c0bfb2164cd8b9a5e78758df'

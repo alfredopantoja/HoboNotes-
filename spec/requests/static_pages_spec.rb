99806b73991164eaad4447802ef60b33ad6a147d
Alfredo Pantoja <chololoy@gmail.com>
2012-05-23 09:15:57 -0500

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Hobo Notes')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Hobo Notes | Home")
+                          :text => "Hobo Notes")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
